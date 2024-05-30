a9345ba19db2c2de0df94608516903d73adce69e
Rami-0 <Ramysh11@icloud.com>
2024-05-30 12:28:27 +0600

    feat: /regestration/otp end point

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,49 +1,69 @@
-const speakeasy = require("speakeasy");
-const bot = require("../bot");
-const UserServices = require("../services/user");
+const speakeasy = require('speakeasy');
+const bot = require('../bot');
+const UserServices = require('../services/user');
 class UserController {
-  async createOTP(req, res) {
-    try {
-      const { INN, PhoneNumber } = req.body;
-      const secret = speakeasy.generateSecret({ length: 6 });
+	static async createOTP(req, res) {
+		try {
+			const { INN, PhoneNumber } = req.body;
 
-      const code = speakeasy.totp({
-        secret: secret.base32,
-        encoding: "base32",
-      });
+			// Generate a TOTP code
+			const secret = speakeasy.generateSecret({ length: 6 });
+			const code = speakeasy.totp({
+				secret: secret.base32,
+				encoding: 'base32',
+			});
+			const message = `Password: ${code}`;
 
-      const message = `Password: ${code}`;
+			let chatId;
+			try {
+				const user = await UserServices.findByINNOrPhoneNumber(INN || PhoneNumber);
+				if (user) {
+					chatId = user.user.ChatID;
+				} else {
+					res.status(404).json({ error: 'User not found' });
+					return;
+				}
+			} catch (error) {
+				console.error('Error finding the user:', error);
+				res.status(500).json({ error: 'Internal Server Error' });
+				return;
+			}
 
-      //TODO: we need to find his chat ID first
-      // if (true == false) {
-      // 	// Check if chatId is available for this user
-      // 	await bot.sendMessage(chatId, message);
-      // } else {
-      // 	console.error(`Chat ID for user ID: ${userId} is not set.`);
-      // }
-      // bot.sendMessage(chatId, message);
-      res.status(200).json(message);
-    } catch (error) {
-      console.error("Error send finding the user:", error);
-      //   res.status(500).json({ error: "Internal Server Error" });
-    }
-  }
-  async init(req, res) {
-    const userData = {
-      PhoneNumber: req.body.PhoneNumber,
-      INN: req.body.INN,
-    };
-    try {
-      const send = await UserServices.createUser(userData); // Use `await` to wait for the promise
-      console.log(send);
-      // bot.sendMessage(chatId, 'Chat created successfully');
-      res.status(200).json({ success: "User created successfully" });
-    } catch (err) {
-      console.log(err);
-      res.status(400).json({ error: err.message });
-      // bot.sendMessage(chatId, 'Chat not created');
-    }
-  }
+			if (chatId) {
+				try {
+					// Send message to the user
+					await bot.sendMessage(chatId, message);
+					res.status(200).json({ message });
+				} catch (sendError) {
+					console.error('Error sending message:', sendError);
+					res.status(500).json({ error: 'Failed to send message' });
+				}
+			} else {
+				console.error('Chat ID is not set for the user.');
+				res.status(400).json({ error: "Chat ID doesn't exist" });
+			}
+		} catch (error) {
+			console.error('Error generating OTP:', error);
+			res.status(500).json({ error: 'Internal Server Error' });
+		}
+	}
+
+	static async init(req, res) {
+		const userData = {
+			PhoneNumber: req.body.PhoneNumber,
+			INN: req.body.INN,
+		};
+		try {
+			const send = await UserServices.createUser(userData); // Use `await` to wait for the promise
+			console.log(send);
+			// bot.sendMessage(chatId, 'Chat created successfully');
+			res.status(200).json({ success: 'User created successfully' });
+		} catch (err) {
+			console.log(err);
+			res.status(400).json({ error: err.message });
+			// bot.sendMessage(chatId, 'Chat not created');
+		}
+	}
 }
 
-module.exports = new UserController();
+module.exports = UserController;
